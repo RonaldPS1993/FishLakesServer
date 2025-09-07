a7e978faa9b31b8f0d17d2749cffb3d8d2ce48a3
RonaldPS1993 <ronaldpenasollet@gmail.com>
2025-09-07 17:53:06 -0400

    Changes to port listen

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const fastify = require("fastify")({
 
 fastify.register(require("./routes/index"), { prefix: "/api" });
 
-fastify.listen({ port: process.env.PORT }, (err, address) => {
+const port = process.env.PORT || 3000;
+
+fastify.listen({ port, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     fastify.log.error(err);
     process.exit(1);
